c86ff349a322614ec23d297f26b2e57326336be7
hkdgnet <hkdgnet@163.com>
2018-07-06 10:24:42 +0800

    code ahuxiao

--- 1.10.cpp
+++ 1.10.cpp
@@ -1,4 +1,4 @@
-﻿//#include <iostream>
+//#include <iostream>
 //
 //int main()
 //{
@@ -233,29 +233,29 @@
 //	return 0;
 //}
 
-#include <iostream>
-int main()
-{
-	int yuanlai = 0, val = 0;
-	if (std::cin >> yuanlai)
-	{
-		int count = 1;
-		for (; std::cin >> val;)
-		{
-			if (val == yuanlai)
-			{
-				++count;
-			}
-			else
-			{
-				std::cout << yuanlai << "有" << count << std::endl;
-				yuanlai = val;
-				count = 1;
-			}
-
-		}
-		std::cout << yuanlai << "有" << count << std::endl;
-	}
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//int main()
+//{
+//	int yuanlai = 0, val = 0;
+//	if (std::cin >> yuanlai)
+//	{
+//		int count = 1;
+//		for (; std::cin >> val;)
+//		{
+//			if (val == yuanlai)
+//			{
+//				++count;
+//			}
+//			else
+//			{
+//				std::cout << yuanlai << "有" << count << std::endl;
+//				yuanlai = val;
+//				count = 1;
+//			}
+//
+//		}
+//		std::cout << yuanlai << "有" << count << std::endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
